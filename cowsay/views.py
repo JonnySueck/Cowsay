47df9d8735fd819832135859fb59057333f3e4f8
Jonathan Sueck <jonathansueck@gmail.com>
2021-02-11 23:05:28 -0500

    text displays when submitted and clears form

--- cowsay/views.py
+++ cowsay/views.py
@@ -1,7 +1,9 @@
+from typing import Text
 from django.shortcuts import redirect, render
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import HttpResponseRedirect, render
+import subprocess, cowsay
 
 # Create your views here.
 def history(request):
@@ -17,8 +19,11 @@ def index_view(request):
             Post.objects.create(
                 text = data['text']
             )
+            last_post = data['text']
             form = PostForm()
-            return render(request, 'index.html', {'form': form})
+            return render(request, 'index.html', {
+                'last_post': last_post,
+                'form': form})
 
     form = PostForm()
     return render(request, 'index.html', {'form': form})
