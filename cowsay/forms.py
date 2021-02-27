5e3ea81b9443a70e36dc36dbf7673c79db1cfe1b
Jonathan Sueck <jonathansueck@gmail.com>
2021-02-27 12:27:16 -0500

    dropdown available for cowsay type, now working on the function to change the cow.

--- cowsay/forms.py
+++ cowsay/forms.py
@@ -2,5 +2,8 @@ from django import forms
 from .models import Post
 
 
-class PostForm(forms.Form):
-    text = forms.CharField(max_length=100)
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('text', 'cowsay_type')
--- cowsay/models.py
+++ cowsay/models.py
@@ -3,7 +3,20 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    DEFAULT = 'Default'
+    TUX = 'Tux'
+    DRAGON = 'Dragon'
+    KITTY = 'Kitty'
+    SKELETON = 'Skeleton'
+    STATUS_CHOICES = [
+        (DEFAULT, 'Default'),
+        (TUX, 'Tux'),
+        (DRAGON, 'Dragon'),
+        (KITTY, 'Kitty'),
+        (SKELETON, 'Skeleton'),
+        ]
     text = models.CharField(max_length=100)
+    cowsay_type = models.CharField(max_length=11, choices=STATUS_CHOICES)
     
     def __str__(self):
         return self.text
